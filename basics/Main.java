c93fad4f16d97d56c1aa2017eb652ddcb4196ab3
MELFI PEREZ <perezmelfi@outlook.com>
2019-08-06 12:44:30 -0700

    Coin flip feature is now complete

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
   public static void main(String[] args) {
     // Feature Pluralize
@@ -9,6 +11,9 @@ public class Main {
 
     int turtleCount = 0;
     System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+    // Feature Flipping Coins
+    flipNHeads(2);
   }
 
   // Feature Pluralize
@@ -20,4 +25,31 @@ public class Main {
     }
     return word;
   }
+
+  // Feature Flipping Coins
+  public static void flipNHeads(int n) {
+    int totalFlips = 0;
+    int headsCounter = 0;
+    double constraint = 0.5;
+
+    while (headsCounter < n) {
+      Random randomNum = new Random();
+      double flip = randomNum.nextDouble();
+
+      if (flip < constraint) {
+        totalFlips++;
+        System.out.println("tails");
+
+      } else if (flip > constraint) {
+        totalFlips++;
+        headsCounter++;
+        System.out.println("heads");
+      }
+
+    }
+    System.out.println("It took " + totalFlips + " flips to flip " + headsCounter + " head in a row.");
+  }
+
+  // Feature Command Line Clock
+
 }
