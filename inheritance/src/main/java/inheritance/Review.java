4902584a08e7b8ce29f39a8e7e5a8aca081f304d
MELFI PEREZ <perezmelfi@outlook.com>
2019-08-14 13:14:48 -0700

    lab completed

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -6,19 +6,33 @@ public class Review {
     public String author;
     public int reviewRating;
     public Restaurant restaurant;
+    public Shop shop;
+    public Theater theater;
+    public String movieTitle;
 
 
 //  Constructor
     public Review(String body, String author, int reviewRating){
-
         this.body = body;
         this.author = author;
         this.reviewRating = reviewRating;
     }
+//  Overloaded Constructor to pass in movie title to include in reviews
+    public Review(String body, String author, int reviewRating, String movieTitle){
+        this.body = body;
+        this.author = author;
+        this.reviewRating = reviewRating;
+        this.movieTitle = movieTitle;
+    }
 
 //  toString method
     public String toString(){
-        return this.body + " I'd give it a " + this.reviewRating +" Out of 5. Reviewed by: " +this.author;
+//  Checks to see if movie title is being passed & builds proper string.
+        if(this.movieTitle == null){
+            return this.body + " I'd give it a " + this.reviewRating +" Out of 5. Reviewed by: " +this.author;
+        } else {
+            return this.body + " I'd give it a " + this.reviewRating +" Out of 5. Reviewed by: " +this.author + ". Saw " + this.movieTitle;
+        }
     }
 }
 
